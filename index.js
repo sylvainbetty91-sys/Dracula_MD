5f58845537f6a303d5b366f8ac8dbf88ac38e4ea
Dracula 509 <sylvainbetty91@gmail.com>
2026-08-15 08:27:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('events').EventEmitter.defaultMaxListeners = 500;
 
 app.use('/code', code);
 app.use('/pair', async (req, res, next) => {
-    res.sendFile(__path + '/main.html')
+    res.sendFile(__path + '/pair.html')
 });
 app.use('/', async (req, res, next) => {
     res.sendFile(__path + '/main.html')
